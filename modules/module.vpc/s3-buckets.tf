4378d5acc837f34ae8073c7db42ba93489fa8156
vivek22117 <vivekmishra22117@gmail.com>
2021-06-20 23:03:39 +0530

    adding github pipeline for ECS cluster deployment

--- modules/module.vpc/s3-buckets.tf
+++ modules/module.vpc/s3-buckets.tf
@@ -42,9 +42,9 @@ resource "aws_s3_bucket" "s3_artifactory_bucket" {
 
   force_destroy = false
 
-  lifecycle {
-    prevent_destroy = "true" // Terraform meta parameter
-  }
+//  lifecycle {
+//    prevent_destroy = "true" // Terraform meta parameter
+//  }
 
   server_side_encryption_configuration {
     rule {
@@ -79,9 +79,9 @@ resource "aws_s3_bucket" "s3_dataLake_bucket" {
 
   force_destroy = false
 
-  lifecycle {
-    prevent_destroy = "true" // Terraform meta parameter
-  }
+//  lifecycle {
+//    prevent_destroy = "true" // Terraform meta parameter
+//  }
 
   server_side_encryption_configuration {
     rule {
