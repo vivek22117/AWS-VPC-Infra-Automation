329154d0da4e059deb4f39a74f069177aa2c8062
vivek22117 <vivekmishra22117@gmail.com>
2021-06-20 14:30:00 +0530

    adding github pipeline for ECS cluster deployment

--- modules/module.eks-cluster/security-groups.tf
+++ modules/module.eks-cluster/security-groups.tf
@@ -70,16 +70,6 @@ resource "aws_security_group_rule" "eks_cluster_inbound_bastion" {
   source_security_group_id = aws_security_group.bastion_host_sg.id
 }
 
-resource "aws_security_group_rule" "eks_cluster_inbound_within_vpc" {
-  type                     = "ingress"
-  description              = "Allow VPC Host to communicate with the cluster API Server"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster.id
-  source_security_group_id = aws_security_group.bastion_host_sg.id
-}
-
 resource "aws_security_group_rule" "eks_cluster_outbound" {
   type                     = "egress"
   description              = "Allow cluster API Server to communicate with the worker nodes"
