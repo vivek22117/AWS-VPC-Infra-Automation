b11db8accb307449e62056a03164ddef14d611c8
vivek22117 <vivekmishra22117@gmail.com>
2021-06-22 13:59:57 +0530

    adding github pipeline for ECS cluster deployment

--- modules/module.eks-cluster/output.tf
+++ modules/module.eks-cluster/output.tf
@@ -64,8 +64,28 @@ output "eks_cluster_endpoint" {
   value = aws_eks_cluster.doubledigit_eks.endpoint
 }
 
+output "eks_cluster_version" {
+  description = "The Kubernetes server version of the cluster"
+  value       = aws_eks_cluster.doubledigit_eks.version
+}
+
+output "eks_cluster_identity_oidc_issuer" {
+  description = "The OIDC Identity issuer for the cluster"
+  value       = join("", aws_eks_cluster.doubledigit_eks.*.identity.0.oidc.0.issuer)
+}
+
+output "eks_cluster_identity_oidc_issuer_arn" {
+  description = "The OIDC Identity issuer ARN for the cluster that can be used to associate IAM roles with a service account"
+  value       = aws_iam_openid_connect_provider.doubledigit_eks.arn
+}
+
 output "eks_cluster_certificate_authority" {
   value = aws_eks_cluster.doubledigit_eks.certificate_authority
+}
+
+output "eks_cluster_certificate_authority_data" {
+  description = "The Kubernetes cluster certificate authority data"
+  value       = local.certificate_authority_data
 }
 
 
