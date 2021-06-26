7f14075152cf940aaf923b1a22baded9fe953b66
vivek22117 <vivekmishra22117@gmail.com>
2021-06-26 20:55:57 +0530

    using s3 module to create resources

---
+++ vpc/s3-buckets/main.tf
@@ -0,0 +1,22 @@
+####################################################
+#        VPC Other Resources Module                #
+####################################################
+module "s3_resources" {
+  source = "../../modules/module.aws-account.buckets"
+
+//  Only used while local run
+  profile        = var.profile
+
+  default_region = var.default_region
+
+  artifactory_bucket_prefix = var.artifactory_bucket_prefix
+  logging_bucket_prefix = var.logging_bucket_prefix
+  dataLake_bucket_prefix = var.dataLake_bucket_prefix
+
+  team        = var.team
+  owner       = var.owner
+  environment = var.environment
+  isMonitoring = var.isMonitoring
+  component = var.component
+  project = var.project
+}
