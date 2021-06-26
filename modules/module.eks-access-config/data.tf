a1bb54cd350ef7d35c52cdef6569ff7b681c4332
vivek22117 <vivekmishra22117@gmail.com>
2021-06-26 20:59:04 +0530

    updating TF data file to use S3 resources

--- modules/module.eks-access-config/data.tf
+++ modules/module.eks-access-config/data.tf
@@ -13,10 +13,21 @@ data "terraform_remote_state" "eks-vpc" {
   }
 }
 
-data "template_file" "ecs_read_only_template" {
+data "terraform_remote_state" "s3_buckets" {
+  backend = "s3"
+
+  config = {
+    profile = var.profile
+    bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
+    key     = "state/${var.environment}/s3-buckets/terraform.tfstate"
+    region  = var.default_region
+  }
+}
+
+data "template_file" "eks_read_only_template" {
   template = file("${path.module}/policy-doc/eks-full-access.json.tpl")
 }
 
-data "template_file" "ecs_full_access_template" {
+data "template_file" "eks_full_access_template" {
   template = file("${path.module}/policy-doc/eks-read-access.json.tpl")
 }
--- modules/module.eks-cluster/data.tf
+++ modules/module.eks-cluster/data.tf
@@ -12,16 +12,5 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-data "terraform_remote_state" "backend" {
-  backend = "s3"
-
-  config = {
-    profile = var.profile
-    bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
-    key     = "state/${var.environment}/backend/terraform.tfstate"
-    region  = var.default_region
-  }
-}
-
 
 data "aws_caller_identity" "current" {}
