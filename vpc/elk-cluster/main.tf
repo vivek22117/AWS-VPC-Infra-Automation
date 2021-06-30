0d0439e61f681b486723a060a74b71cd111970f7
vivek22117 <vivekmishra22117@gmail.com>
2021-06-30 22:16:21 +0530

    formatting TF script

--- vpc/elk-cluster/main.tf
+++ vpc/elk-cluster/main.tf
@@ -4,27 +4,27 @@
 module "doubledigit-logging-es" {
   source = "../../modules/module-elk-stack"
 
-  profile = var.profile
+  profile        = var.profile
   environment    = var.environment
   default_region = var.default_region
 
-  volume_type = var.volume_type
-  indices_query_bool_max_clause_count = var.indices_query_bool_max_clause_count
+  volume_type                            = var.volume_type
+  indices_query_bool_max_clause_count    = var.indices_query_bool_max_clause_count
   rest_action_multi_allow_explicit_index = var.rest_action_multi_allow_explicit_index
-  ingress_allow_security_groups = var.ingress_allow_security_groups
-  sg_name = var.sg_name
-  es_domain_name = var.es_domain_name
-  elasticsearch_version = var.elasticsearch_version
-  encryption_enabled = var.encryption_enabled
-  instance_type = var.instance_type
-  instance_count = var.instance_count
-  zone_awareness_enabled = var.zone_awareness_enabled
-  volume_size = var.volume_size
-  snapshot_start = var.snapshot_start
+  ingress_allow_security_groups          = var.ingress_allow_security_groups
+  sg_name                                = var.sg_name
+  es_domain_name                         = var.es_domain_name
+  elasticsearch_version                  = var.elasticsearch_version
+  encryption_enabled                     = var.encryption_enabled
+  instance_type                          = var.instance_type
+  instance_count                         = var.instance_count
+  zone_awareness_enabled                 = var.zone_awareness_enabled
+  volume_size                            = var.volume_size
+  snapshot_start                         = var.snapshot_start
 
 
-  team  = var.team
+  team      = var.team
   component = var.component
-  project = var.project
+  project   = var.project
 
 }
