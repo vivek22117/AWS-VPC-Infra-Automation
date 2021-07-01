0021cdb1087d1dccb970966249e9d1a69052bbe0
vivek22117 <vivekmishra22117@gmail.com>
2021-07-01 23:08:34 +0530

    adding github pipeline for ECS cluster deployment

---
+++ vpc/eks-access-config/main.tf
@@ -0,0 +1,16 @@
+####################################################
+#        Dev EKS-VPC module configuration          #
+####################################################
+module "eks-vpc" {
+  source = "../../modules/module.eks-access-config"
+
+  environment    = var.environment
+  default_region = var.default_region
+
+
+  eks_bastion_name_prefix = var.eks_bastion_name_prefix
+  bastion_instance_type = var.bastion_instance_type
+  default_cooldown = var.default_cooldown
+
+
+}
