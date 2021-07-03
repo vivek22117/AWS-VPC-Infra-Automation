2f9b2637fdd9faa05b5bc5201807a04764910959
vivek22117 <vivekmishra22117@gmail.com>
2021-07-03 18:50:18 +0530

    adding github pipeline for ECS cluster deployment

--- vpc/eks/output.tf
+++ vpc/eks/output.tf
@@ -1,3 +1,9 @@
+output "eks_cluster_id" {
+  description = "The name of the cluster"
+  value       = module.eks-vpc.eks_cluster_id
+}
+
+
 output "eks_cluster_arn" {
   description = "The Amazon Resource Name (ARN) of the cluster"
   value       = module.eks-vpc.eks_cluster_arn
