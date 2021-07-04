00735242d334fec5a9cc92983ae113e797526cfd
vivek22117 <vivekmishra22117@gmail.com>
2021-07-04 09:31:32 +0530

    installing jq in bastion host

--- modules/module.eks-access-config/data-scripts/configure-eks.sh
+++ modules/module.eks-access-config/data-scripts/configure-eks.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
 
+
+sudo yum install jq -y
+
+
+cd tmp/
+
 echo 'Applying Auth ConfigMap with kubectl...'
 
 aws s3 cp s3://${artifactory_bucket_name}/deploy/eks/config-auth.yaml . --region ${default_region}
 
-aws sts assume-role --role-arn ${eks_create_role_arn} --role-session-name Cluster-Config
+TEMP=$(aws sts assume-role --role-arn ${eks_create_role_arn} --role-session-name Cluster-Config)
+aws configure set profile.eks-creator.aws_access_key $(echo $TEMP | jq -r .Credentials.AccessKeyId)
+aws configure set profile.eks-creator.aws_secret_access_key $(echo $TEMP | jq -r .Credentials.SecretAccessKey)
+aws configure set profile.eks-creator.aws_session_token $(echo $TEMP | jq -r .Credentials.SessionToken)
+aws configure set profile.eks-creator.region ${default_region}
 
-aws eks update-kubeconfig --name=${cluster_name} --region=${default_region}
 
-kubectl version
+aws eks update-kubeconfig --name=${cluster_name} --region=${default_region} --role-arn ${eks_create_role_arn}
 
 kubectl apply -f config-auth.yaml
 
