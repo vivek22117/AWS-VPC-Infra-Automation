0c2399e3598d6377806ec22409d6be6622ba8a10
vivek22117 <vivekmishra22117@gmail.com>
2021-07-13 21:23:08 +0530

    updating output TF for IRSA moduel

--- vpc/eks-irsa/output.tf
+++ vpc/eks-irsa/output.tf
@@ -1,21 +1,19 @@
 output "name" {
-  value       = aws_iam_role.irsa_role.*.name
-  description = "The name of generated IAM role"
+  value       = module.eks_irsa.name
 }
 
 output "arn" {
-  value       = aws_iam_role.irsa_role.*.arn
-  description = "The ARN of generated IAM role"
+  value       = module.eks_irsa.arn
 }
 
-output "kubecli" {
+output "kubectl_cli" {
   value = (var.enabled ? join(" ", [
     format("kubectl -n %s create sa %s", var.namespace, var.service_account),
     "&&",
     format("kubectl -n %s annotate sa %s %s",
     var.namespace,
     var.service_account,
-    join("=", ["eks.amazonaws.com/role-arn", aws_iam_role.irsa_role.0.arn])
+    join("=", ["eks.amazonaws.com/role-arn", module.eks_irsa.kubectl_cli])
     ),
   ]) : null)
   description = "The kubernetes configuration file for creating IAM role with service account"
