9d870d16e9e29c1307b5b0b29613646c1d4dd6e3
vivek22117 <vivekmishra22117@gmail.com>
2021-07-13 21:24:08 +0530

    formatting TF script

--- vpc/eks-irsa/main.tf
+++ vpc/eks-irsa/main.tf
@@ -4,17 +4,17 @@
 module "eks_irsa" {
   source = "../../modules/module.eks-irsa"
 
-  component = var.component
-  environment = var.environment
+  component      = var.component
+  environment    = var.environment
   default_region = var.default_region
-  isMonitoring = var.isMonitoring
-  project = var.project
-  owner = var.owner
-  team = var.team
+  isMonitoring   = var.isMonitoring
+  project        = var.project
+  owner          = var.owner
+  team           = var.team
 
-  enabled = var.enabled
-  namespace = var.namespace
-  oidc_arn = var.oidc_arn
-  oidc_url = var.oidc_url
+  enabled         = var.enabled
+  namespace       = var.namespace
+  oidc_arn        = var.oidc_arn
+  oidc_url        = var.oidc_url
   service_account = var.service_account
 }
