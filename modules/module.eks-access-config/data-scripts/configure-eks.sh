1c83155086d89f50e3db173954b2a9a1f915564a
vivek22117 <vivekmishra22117@gmail.com>
2021-07-31 21:12:58 +0530

    adding github pipeline for ECS cluster deployment

--- modules/module.eks-access-config/data-scripts/configure-eks.sh
+++ modules/module.eks-access-config/data-scripts/configure-eks.sh
@@ -3,23 +3,37 @@
 
 
 sudo yum install jq -y
-
-
 cd tmp/
 
-echo 'Applying Auth ConfigMap with kubectl...'
+function download_eks_auth() {
+  echo ================== Download EKS auth config ===============================
+  echo 'Applying Auth ConfigMap with kubectl...'
+  aws s3 cp s3://${artifactory_bucket_name}/deploy/eks/config-auth.yaml . --region ${default_region}
 
-aws s3 cp s3://${artifactory_bucket_name}/deploy/eks/config-auth.yaml . --region ${default_region}
+}
 
-TEMP=$(aws sts assume-role --role-arn ${eks_create_role_arn} --role-session-name Cluster-Config)
-aws configure set profile.eks-creator.aws_access_key $(echo $TEMP | jq -r .Credentials.AccessKeyId)
-aws configure set profile.eks-creator.aws_secret_access_key $(echo $TEMP | jq -r .Credentials.SecretAccessKey)
-aws configure set profile.eks-creator.aws_session_token $(echo $TEMP | jq -r .Credentials.SessionToken)
-aws configure set profile.eks-creator.region ${default_region}
+function create_credentials() {
+  echo ================== Configure Credentials ===============================
+
+  TEMP=$(aws sts assume-role --role-arn ${eks_create_role_arn} --role-session-name Cluster-Config)
+  aws configure set profile.eks-creator.aws_access_key $(echo $TEMP | jq -r .Credentials.AccessKeyId)
+  aws configure set profile.eks-creator.aws_secret_access_key $(echo $TEMP | jq -r .Credentials.SecretAccessKey)
+  aws configure set profile.eks-creator.aws_session_token $(echo $TEMP | jq -r .Credentials.SessionToken)
+  aws configure set profile.eks-creator.region ${default_region}
+}
 
 
-aws eks update-kubeconfig --name ${cluster_name} --region ${default_region} --role-arn ${eks_create_role_arn} --profile eks-creator
+function update_and_apply_config() {
+  echo ================== Update & Apply Config ===============================
 
-kubectl apply -f config-auth.yaml
+  aws eks update-kubeconfig --name ${cluster_name} --region ${default_region} --role-arn ${eks_create_role_arn} --profile eks-creator
+  kubectl apply -f config-auth.yaml
+  echo 'Applied Auth ConfigMap with kubectl'
+}
 
-echo 'Applied Auth ConfigMap with kubectl'
+
+download_eks_auth
+create_credentials
+update_and_apply_config
+
+
