9b806b8384fbc6085322f142a0d54a6b62db96c4
vivek22117 <vivekmishra22117@gmail.com>
2021-08-03 14:03:42 +0530

    adding s3 bucket tf script

---
+++ s3-automation/s3.tf
@@ -0,0 +1,13 @@
+resource "aws_cloudformation_stack" "s3_stack" {
+  count = length(compact(split(",",var.s3_bucket_with_replication_dst_region))) > 0 ? 1 : 0
+
+  name          = "${var.s3_buckt_stack_name}-${var.environment}"
+
+  template_body = templatefile("${path.module}/script/s3-bucket-ctf.json.tpl", {
+    s3_bucket_name = "${var.bucket_name}-${var.global_region}"
+    role_arn = var.role_arn
+    aws_region = var.var.s3_bucket_with_replication_dst_region[count.index]
+  })
+
+  tags = merge(local.common_tags, map("Name", "Notification-API-SNS"))
+}
